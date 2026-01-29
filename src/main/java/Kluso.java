7d2dd3d199c6dc5477291bb3f13e200faac54319
aqfw <116161343+aqfw@users.noreply.github.com>
2026-01-29 23:27:47 +0800

    Change Kluso's personality and provided echo function

--- src/main/java/Kluso.java
+++ src/main/java/Kluso.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kluso {
     public static void main(String[] args) {
         String oldLogo = " ____        _        \n"
@@ -5,23 +7,35 @@ public class Kluso {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
         String lineBreak = "_______________________"
                 + "________________________________"
                 + "_____\n";
-
         String newName = "Kluso";
-        String greetingText = "Hello! I'm " + newName
-                + "\nWhat can I do for you?\n";
-        String quittingText = "Bye. Hope to see you again "
-                + "soon!\n";
-        
+        String greetingText = newName + " single ship, checking in!\n"
+                + "Tasking?\n";
+        String quittingText = newName + " is RTB!\n";
         
         System.out.println(lineBreak
                 + greetingText
-                + lineBreak
-                + quittingText
                 + lineBreak);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean windowOpen = true;
+        while (windowOpen == true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("exit")) {
+                System.out.println(lineBreak + quittingText + lineBreak);
+                windowOpen = false;
+            } else {
+                System.out.println(lineBreak + input + "\n" + lineBreak);
+                System.out.println();
+            }
+        }
+
+        scanner.close();
+
     }
 
 }
